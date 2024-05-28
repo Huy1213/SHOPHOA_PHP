d7274c6df19755d3f4219c5163d3341b33655583
duongbaduy <riox.doubled@gmail.com>
2024-05-28 18:41:04 +0700

    them layout

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,14 @@ from django.urls import path
 from . import views # call to url_shortener/views.py
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('contact_us/', views.about_us, name='contact_us'),
+    path('my_account/', views.my_account, name='my_account'),
+    path('shop/', views.shop, name='shop'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('forgotpassword/', views.forgotpassword, name='forgotpassword'),
+    path('product_details/', views.product_details, name='product_details'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,29 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'page/home.html')
 # Create your views here.
+def login(request):
+    return render(request, 'page/login.html')
+
+def register(request):
+    return render(request, 'page/register.html')
+
+def about_us(request):
+    return render(request, 'page/contact_us.html')
+
+def my_account(request):
+    return render(request, 'page/my_account.html')
+
+def shop(request):
+    return render(request, 'page/shop.html')
+
+def cart(request):
+    return render(request, 'page/cart.html')
+
+def checkout(request):
+    return render(request, 'page/checkout.html')
+
+def forgotpassword(request):
+    return render(request, 'page/forgotpassword.html')
+
+def product_details(request):
+    return render(request, 'page/product_details.html')
